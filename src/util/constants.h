ce4876da92edae57c76aa51c40593e2c710d1b71
techgineer <ahdavis13@gmail.com>
2017-03-10 11:46:33 -0600

    Bug fix

--- src/util/constants.h
+++ src/util/constants.h
@@ -11,16 +11,16 @@
 #define CONST_H_INC
 
 //screen size constants
-const int SCREEN_WIDTH = 640; //the width of the screen
-const int SCREEN_HEIGHT = 480; //the height of the screen
+#define SCREEN_WIDTH 640 //the width of the screen
+#define SCREEN_HEIGHT 480 //the height of the screen
 
 //the header text for the game window
-const char* WIN_HEADER = "Validian Chronicles I: Giant's Bane";
+#define WIN_HEADER "Validian Chronicles I: Giant's Bane"
 
 //the version string for the game
-const char* VERSION = "0.1.0 alpha";
+#define VERSION "0.1.0 alpha"
 
 //the temporary message displayed during development
-const char* DEV_MSG = "Giant's Bane is currently in development.";
+#define DEV_MSG "Giant's Bane is currently in development."
 
 #endif
