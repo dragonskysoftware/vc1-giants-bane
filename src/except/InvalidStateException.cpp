9afb08de010c5380b4acc92d4f43cd9d8375049c
techgineer <ahdavis13@gmail.com>
2017-03-11 11:37:58 -0600

    Added invalid state exception class

---
+++ src/except/InvalidStateException.cpp
@@ -0,0 +1,58 @@
+/*
+ * InvalidStateException.cpp
+ * Implements an exception that is thrown when an object enters an invalid operational state
+ * Created by Andrew Davis
+ * Created on 3/11/2017
+ * Open source (GPL license)
+ */
+
+//include header
+#include "InvalidStateException.h"
+
+//constructor
+InvalidStateException::InvalidStateException(const char* msg)
+	: errorString(msg) //init the error message string
+{
+	this->errorString = "InvalidStateException thrown! Message: " + this->errorString; //append a prefix to the error string
+}
+
+//destructor
+InvalidStateException::~InvalidStateException() {
+	this->errorString.clear(); //clear the error string
+}
+
+//copy constructor
+InvalidStateException::InvalidStateException(const InvalidStateException& ise)
+	: errorString(ise.errorString) //copy the error string
+{
+	//no code needed
+}
+
+//move constructor
+InvalidStateException::InvalidStateException(InvalidStateException&& ise)
+	: errorString(ise.errorString) //move the error string
+{
+	ise.errorString.clear(); //empty the temporary's error string
+}
+
+//assignment operator
+InvalidStateException& InvalidStateException::operator=(const InvalidStateException& src) {
+	this->errorString = src.errorString; //assign the error string
+	return *this; //return the object
+}
+
+//move operator
+InvalidStateException& InvalidStateException::operator=(InvalidStateException&& src) {
+	this->errorString = src.errorString; //move the error string
+	src.errorString.clear(); //empty the temporary's error string
+	return *this; //return the object
+}
+
+//overridden what() method - called when the exception is thrown
+const char* InvalidStateException::what() const throw() {
+	return this->errorString.c_str(); //return the error string as a C string
+}
+
+//end of implementation
+
+
---
+++ src/except/InvalidStateException.h
@@ -0,0 +1,49 @@
+/*
+ * InvalidStateException.h
+ * Declares an exception that is thrown when an object enters an invalid operational state
+ * Created by Andrew Davis
+ * Created on 3/11/2017
+ * Open source (GPL license)
+ */
+
+//disallow reinclusion
+#ifndef INVAL_ST_EXCE
+#define INVAL_ST_EXCE
+
+//includes
+#include <exception>
+#include <string>
+
+//class declaration
+class InvalidStateException final : public std::exception
+{
+	//public fields and methods
+	public:
+		//constructor
+		InvalidStateException(const char* msg);
+
+		//destructor
+		~InvalidStateException();
+
+		//copy constructor
+		InvalidStateException(const InvalidStateException& ise);
+
+		//move constructor
+		InvalidStateException(InvalidStateException&& ise);
+
+		//assignment operator
+		InvalidStateException& operator=(const InvalidStateException& src);
+
+		//move operator
+		InvalidStateException& operator=(InvalidStateException&& src);
+
+		//overridden what() method - called when the exception is thrown
+		const char* what() const throw() override;
+	
+	//private fields and methods
+	private:
+		//fields
+		std::string errorString; //the string containing the error message
+};
+
+#endif
