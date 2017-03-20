6bb1ab8e0e6126d23340cde56f7bd2b75fc43373
techgineer <ahdavis13@gmail.com>
2017-03-20 12:53:37 -0500

    Added string duplication function

---
+++ src/util/strdup.cpp
@@ -0,0 +1,20 @@
+/*
+ * strdup.cpp
+ * Implements a function that duplicates character strings
+ * Created by Andrew Davis
+ * Created on 3/16/2017
+ * Open source (GPL license)
+ */
+
+//include header
+#include "strdup.h"
+
+//strdup function implementation - duplicates a character string
+char* strdup(const char* str) noexcept {
+	size_t len = strlen(str) + 1; //get the length of the supplied string plus one
+	char* ret = new char[len]; //allocate a character string with the calculated length
+	strcpy(ret, str); //copy in the supplied string
+	return ret; //return the duplicated, initialized string
+}
+
+//end of implementation
---
+++ src/util/strdup.h
@@ -0,0 +1,20 @@
+/*
+ * strdup.h
+ * Declares a function that duplicates character strings
+ * Created by Andrew Davis
+ * Created on 3/16/2017
+ * Open source (GPL license)
+ */
+
+//disallow reinclusion
+#ifndef STRDUP_H_INC
+#define STRDUP_H_INC
+
+//include
+#include <cstring>
+#include <cstdlib>
+
+//function declaration
+char* strdup(const char* str) noexcept;
+
+#endif
