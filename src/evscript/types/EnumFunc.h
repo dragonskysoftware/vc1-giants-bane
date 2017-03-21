155a19fb4193998a7b8058bd470e21d1f552e4c6
techgineer <ahdavis13@gmail.com>
2017-03-21 12:09:11 -0500

    Added enumeration for built-in function IDs

---
+++ src/evscript/types/EnumFunc.h
@@ -0,0 +1,20 @@
+/*
+ * EnumFunc.h
+ * Enumerates different built-in function IDs available to EvScript
+ * Created by Andrew Davis
+ * Created on 3/21/2017
+ * Open source (GPL license)
+ */
+
+//disallow reinclusion
+#ifndef ENUM_FUNC_H
+#define ENUM_FUNC_H
+
+//enum definition
+enum class EnumFunc {
+	SQRT, //square root
+	EXP, //exponentiate
+	LOG, //logarithm
+};
+
+#endif
