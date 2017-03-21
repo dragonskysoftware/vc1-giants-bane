1cff5621407988b06355ee9bff31ac96284f3f12
techgineer <ahdavis13@gmail.com>
2017-03-21 12:09:39 -0500

    Added enumeration for game call IDs

---
+++ src/evscript/types/EnumCall.h
@@ -0,0 +1,18 @@
+/*
+ * EnumCall.h
+ * Enumerates different game calls that can be made by EvScript scripts
+ * Created by Andrew Davis
+ * Created on 3/21/2017
+ * Open source (GPL license)
+ */
+
+//disallow reinclusion
+#ifndef ENUM_CALL_H
+#define ENUM_CALL_H
+
+//enum definition
+enum class EnumCall {
+	SHUTDOWN, //shut down game
+};
+
+#endif
