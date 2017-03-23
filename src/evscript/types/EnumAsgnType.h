3ff3cdfda4765b431754b94049b2142ffbc1006d
techgineer <ahdavis13@gmail.com>
2017-03-23 17:18:33 -0500

    Added assignment type enum

---
+++ src/evscript/types/EnumAsgnType.h
@@ -0,0 +1,19 @@
+/*
+ * EnumAsgnType.h
+ * Enumerates different types of assignments in EvScript
+ * Created by Andrew Davis
+ * Created on 3/23/2017
+ * Open source (GPL license)
+ */
+
+//disallow reinclusion
+#ifndef ENUM_ASGN_H
+#define ENUM_ASGN_H
+
+//enum definition
+enum class EnumAsgnType {
+	VAL, //value type
+	ARR //array type
+};
+
+#endif
