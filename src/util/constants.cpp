6d8b9446925898b40deac979e25c726b3ceddee7
techgineer <ahdavis13@gmail.com>
2017-04-11 12:21:14 -0500

    Changed window size

--- src/util/constants.cpp
+++ src/util/constants.cpp
@@ -10,8 +10,8 @@
 #include "constants.h"
 
 //screen size constants
-const int SCREEN_WIDTH = 640; //the width of the screen
-const int SCREEN_HEIGHT = 480; //the height of the screen
+const int SCREEN_WIDTH = 860; //the width of the screen
+const int SCREEN_HEIGHT = 700; //the height of the screen
 
 //the header text for the game window
 const char* WIN_HEADER = "Validian Chronicles I: Giant's Bane";
